ac025c42deb337bae1122d7cc7b09a3f6232acfa
Unknown <yifengyang01@gmail.com>
2018-12-16 22:36:11 -0500

    added decryption method

--- crypto/src/main/java/com/yfy/beem/client/crypto/RSACryptographyService.java
+++ crypto/src/main/java/com/yfy/beem/client/crypto/RSACryptographyService.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
+import java.security.KeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -23,25 +26,37 @@ public class RSACryptographyService
     private final RSAPublicKey publicKey;
     private final RSAPrivateKey privateKey;
 
-
+    /**
+     * Decrypt using
+     * */
     @Override
     public String encrypt(String original) {
         try {
             Cipher cipher  = Cipher.getInstance(RSA);
+            log.info("cipher = ", cipher);
             cipher.init(Cipher.ENCRYPT_MODE,publicKey);
             byte[] encrypted = cipher.doFinal(original.getBytes());
             return new String(encrypted);
         } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
             log.info("error: {}", e);
         } catch (InvalidKeyException e) {
-
-        } catch (Exception e) {
-
+            log.info("invalid key: {}", e);
+        }  catch (GeneralSecurityException e) {
+            log.info("security exception: {}", e);
         }
+        return null;
     }
 
     @Override
     public String decrypt(String encrypted) {
+        try {
+            Cipher cipher = Cipher.getInstance(RSA);
+            cipher.init(Cipher.DECRYPT_MODE,privateKey);
+            byte[] decrypted = cipher.doFinal(encrypted.getBytes());
+            return new String(decrypted);
+        } catch (GeneralSecurityException e) {
+            log.info("security exception: {}", e);
+        }
         return null;
     }
 }
